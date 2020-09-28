804ab8136bcd600e00da13d97feebc97cf5a1989
Alex Yu <alex@NEBULA.milkyway.net>
2020-09-27 23:50:54 -0400

    first commit

---
+++ src/exercise3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exercise3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Height : ");
+        double heightInInches = in.nextDouble();
+        System.out.print("Width  : ");
+        double widthInInches = in.nextDouble();
+
+        in.close();
+
+        double widthInMillimeters = widthInInches * 2.54 * 10;
+        double heightInMillimeters = heightInInches * 2.54 * 10;
+
+        double result = widthInMillimeters * heightInMillimeters;
+
+        System.out.println(result + " square millimeters.");
+    }
+}
