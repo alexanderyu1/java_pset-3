ec12b562d4d6b17d5a735f6f71af30a7ae63287e
Alex Yu <alex@NEBULA.milkyway.net>
2020-10-06 23:52:27 -0400

    fix

--- src/exercise1.java
+++ src/exercise1.java
@@ -1,9 +1,10 @@
 public class exercise1 {
     public static void main(String[] args) {
-        System.out.println("H E L L O\n" +
+        System.out.println(
+                "H E L L O\n" +
                 "E       A\n" +
                 "L       P\n" +
-                "L       C \n" +
+                "L       C\n" +
                 "O A P C S");
     }
 }
